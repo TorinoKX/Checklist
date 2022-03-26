b21936eecd7da4a146b20c9d3c16ba71cc4561b8
Zak Cobham-Davis <zak.cobham-davis@griffithuni.edu.au>
2022-03-26 10:17:48 +1000

    Initial Commit

---
+++ Checklist/ChecklistApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChecklistApp.swift
+//  Checklist
+//
+//  Created by zak on 26/3/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChecklistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Checklist/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Checklist
+//
+//  Created by zak on 26/3/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
